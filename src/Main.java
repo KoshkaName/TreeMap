802f57407731555bbfd27ac3686ab78eac30c1bf
KoshkaName <114123386+KoshkaName@users.noreply.github.com>
2022-11-03 19:46:37 +1000

    eraser added

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,23 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
         List<Person> people = new ArrayList<>();
         people.add(new Person("Милина", "Якименко", 12));
+        people.add(new Person("Ольга", "Векю", 16));
         people.add(new Person("Олеся", "Галлямова Якименко", 20));
         people.add(new Person("Алексей", "Серый Волк Пушистый", 19));
         people.add(new Person("Юрий", "Серый Якименко", 39));
 
         Collections.sort(people, new PersonsComparator(3));
         System.out.println(people);
+
+        Predicate<Person> years18 = (person) -> (person.getAge() < 18);
+        people.removeIf(years18);
+        System.out.println("Люди младше 18 лет удалены, остались старше 18: " + people);
     }
 }
 
